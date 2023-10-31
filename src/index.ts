c7408ec4fe7a64eba9f4f88364b994d12cc1fcce
Elvis Hernandez <elvishernandezdev@gmail.com>
2023-10-31 13:18:54 -0400

    manully catch and handle errors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { ErrorHandlingMiddleware } from "./middlewares/errors";
+import { ErrorHandler } from "./services/ErrorHandler";
 // import { PrismaClient } from "@prisma/client";
 
 const app = express();
@@ -6,6 +8,14 @@ const port = 3000;
 // const prisma = new PrismaClient();
 
 app.use(express.json());
+
+app.get("/test", (req, res, next) => {
+  console.log(process.env.NODE_ENV);
+  return next(new ErrorHandler("Test error", 500));
+  //   res.json({ msg: "hello, world" });
+});
+
+app.use(ErrorHandlingMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
