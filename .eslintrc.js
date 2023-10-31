375cc7bc3096cbbb7a06c7c01f0ed0a1911527b5
Elvis Hernandez <elvishernandezdev@gmail.com>
2023-10-31 12:16:13 -0400

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+};
